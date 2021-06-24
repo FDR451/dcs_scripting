785e65527075daf1fb10971dacb6628721b191d6
FDR451 <relativegal@gmail.com>
2021-06-25 00:27:00 +0200

    unification

--- hindsight/assert.lua
+++ hindsight/assert.lua
@@ -1,3 +1,3 @@
-assert(loadfile("C:\\Users\\frueg\\Saved Games\\DCS.openbeta\\Missions\\hindsight\\simple.lua"))() 
+assert(loadfile("C:\\Users\\frueg\\Saved Games\\DCS.openbeta\\Missions\\dcs_scripting\\simple.lua"))() 
 
-assert(loadfile("C:\\Users\\frueg\\Saved Games\\DCS.openbeta\\Missions\\hindsight\\hindsight.lua"))() 
+assert(loadfile("C:\\Users\\frueg\\Saved Games\\DCS.openbeta\\Missions\\dcs_scripting\\hindsight\\hindsight.lua"))() 
