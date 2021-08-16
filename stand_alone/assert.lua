7e64a0ccec8042d1cac275cd3bca9f8991db803b
FDR451 <relativegal@gmail.com>
2021-08-16 18:16:39 +0200

    .-

--- stand_alone/assert.lua
+++
@@ -1,3 +0,0 @@
-assert(loadfile("C:\\Users\\frueg\\Saved Games\\DCS.openbeta\\Missions\\dcs_scripting\\stand_alone\\attackMove.lua"))()
-
-assert(loadfile("C:\\Users\\frueg\\Saved Games\\DCS.openbeta\\Missions\\dcs_scripting\\stand_alone\\attackMove.lua"))()
