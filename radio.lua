15e86cefc01bbd101bff0b69bf42fba8d5d52f96
FDR451 <68690858+FDR451@users.noreply.github.com>
2022-08-15 05:58:28 +0200

    Update radio.lua

--- radio.lua
+++ radio.lua
@@ -2,14 +2,19 @@ radio = {}
 
 radio.messages = {
 
-    [1] = "debug: we see you, starting to move",
-    [2] = "debug: we are taking fire from the north and the east",
-    [2] = "debug: the attackers from the city are retreating",
-    [3] = "debug: the attackers in the forest are retreating",
+    [1] = "placeholder: we see you, starting to move",
+
+    [2] = "placeholder: we have spotted a sus car parked 1km in front of the convoy",
+    [3] = "placeholder: show of force success they are leaving",
+
+    [4] = "placeholder: the attackers from the city are retreating",
+    [5] = "placeholder: the attackers from the forest are retreating",
+    [6] = "placeholder: no more fighting, evacuate the wounded",
 
 }
 
-function radio.notify(messageNumber)
-    trigger.action.outText(radio.messages.messageNumber, 10)
+function radio.notify(messageKey)
+    --print(radio.messages.messageNumber)
+    trigger.action.outText(radio.messages[messageKey], 10)
     trigger.action.outSound("Alert.ogg")
 end
